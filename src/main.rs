128c84e2ebe59169f303aa1918df61f1e2cdb0b5
Maxuss <maxus.info.mail@gmail.com>
2023-08-31 10:41:23 +0300

    some basic server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::str::FromStr;
+use std::{str::FromStr, time::Duration};
 
 use reqwest::{
     header::{HeaderMap, HeaderName, HeaderValue, USER_AGENT},
-    Client,
+    Client, Request,
 };
-use rspotify::{prelude::*, scopes, AuthCodePkceSpotify, Credentials, OAuth};
+use rspotify::{prelude::*, scopes, AuthCodePkceSpotify, Credentials, OAuth, Token};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -18,11 +22,48 @@ async fn main() -> anyhow::Result<()> {
 
     let url = spotify.get_authorize_url(None)?;
 
-    spotify.prompt_for_token(&url).await?;
+    println!("{url}");
+    let (code, oauth_state) = server_oneshot().await?;
+    if oauth.state != oauth_state {
+        println!("Failed to login! Something wrong with OAuth?");
+        return Ok(());
+    }
+    spotify.request_token(&code).await?;
 
     let history = spotify.current_playback(None, None::<Vec<_>>).await?;
 
     println!("Response: {history:#?}");
 
     Ok(())
+}
+
+async fn server_oneshot() -> anyhow::Result<(String, String)> {
+    let tcp_listener = TcpListener::bind("127.0.0.1:8888").await?;
+
+    // Only accept a single connection
+    let (mut client, _) = tcp_listener.accept().await?;
+
+    // 327 bytes are:
+    // GET /callback?code=   :: 19 bytes
+    // <Auth code>           :: 276 bytes
+    // &state=               :: 7 bytes
+    // <OAuth state>         :: 16 bytes
+    let mut necessary_info = [0u8; 318];
+
+    client.read_exact(&mut necessary_info).await?;
+    client
+        .write_all(
+            br#"HTTP/1.1 200 OK
+content-length: 39
+content-type: text/plain
+
+Success! You may now close this window."#,
+        )
+        .await?;
+
+    let data = &String::from_utf8(necessary_info.to_vec()).unwrap()[19..];
+    let code = &data[..276];
+    let oauth_state = &data[283..];
+
+    Ok((code.to_owned(), oauth_state.to_owned()))
 }
